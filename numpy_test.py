a631525106b98dcd1df7a2a78fc3e3b87b5abb7b
agsheves <andrew@tarjumansolutions.com>
2023-10-14 14:36:51 -0400

    Creating the repo

---
+++ numpy_test.py
@@ -0,0 +1,15 @@
+#numpy_test.py
+
+import numpy as np
+
+# Create a numpy array
+arr = np.array([1, 2, 3, 4, 5])
+
+# Perform some operations
+mean = np.mean(arr)
+std_dev = np.std(arr)
+
+# Print the results
+print("Array:", arr)
+print("Mean:", mean)
+print("Standard Deviation:", std_dev)
