82c7815ad821e4ddc5015266f621a9fe3849b04d
agsheves <andrew@tarjumansolutions.com>
2023-10-14 22:24:39 -0400

    More news edits

--- llama_index.py
+++ llama_index.py
@@ -74,7 +74,7 @@ def get_country_news_newsdata(api_key, country_name):
 
 def write_news_summary(country_articles_list):
     news_summary = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo-16k",
         temperature=0,
         messages=[
             {
